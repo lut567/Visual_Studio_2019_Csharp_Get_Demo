57e049d0c23296ca71c6feae0f5184d69f7099e6
Sluttmann <Sluttmann@mail.usf.edu>
2019-08-29 14:41:00 -0800

    Added Nested If inside a while inside another while loop.

--- Program.cs
+++ Program.cs
@@ -14,10 +14,25 @@ namespace Visual_Studio_2019_Csharp_Get_Demo
             while (exit == "Yes" || exit == "yes" || exit == "Y" || exit == "y")
             {
                 Console.WriteLine("\nHello World!");
+
+                
                 Console.Write("What is your name?: ");
                 name = Console.ReadLine();
-                Console.WriteLine("\nHello, " + name + "! You are not as bad is I thought you were!");
 
+                while (name == "")
+                {
+                    Console.Write("Really now? What is your name: ");
+                    name = Console.ReadLine();
+                    if (name == "")
+                    {
+                        Console.WriteLine("Well OK then, since you dont want to play along...");
+                        Environment.Exit(0);
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nHello, " + name + "! You are not as bad is I thought you were!\n");
+                    }
+                }
                 Console.Write("Do you want to loop this program again?: ");
                 exit = Console.ReadLine();
             }                       
