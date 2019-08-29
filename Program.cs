754023abfff8602fdcdd7670c39fffb98eaf1702
Sluttmann <Sluttmann@mail.usf.edu>
2019-08-29 14:25:51 -0800

    Made basic program with while loop.

--- Program.cs
+++ Program.cs
@@ -6,8 +6,22 @@ namespace Visual_Studio_2019_Csharp_Get_Demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Changes"); 
+            string name = "";
+            String exit = "Yes";
+
+            Console.WriteLine("Hopefuly I remember how to code in C#.");
+
+            while (exit == "Yes" || exit == "yes" || exit == "Y" || exit == "y")
+            {
+                Console.WriteLine("\nHello World!");
+                Console.Write("What is your name?: ");
+                name = Console.ReadLine();
+                Console.WriteLine("\nHello, " + name + "! You are not as bad is I thought you were!");
+
+                Console.Write("Do you want to loop this program again?: ");
+                exit = Console.ReadLine();
+            }                       
+            Environment.Exit(0);
         }
     }
 }
