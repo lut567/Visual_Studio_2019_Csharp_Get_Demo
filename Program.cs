e1cce7de4adc733978d70c1503e6d5f8c83b908e
Sluttmann <Sluttmann@mail.usf.edu>
2019-08-29 14:11:19 -0800

    Commit the changes

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Visual_Studio_2019_Csharp_Get_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
